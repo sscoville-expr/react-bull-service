2c177fa312c0eefa484654bfbe9a885b82d9f55e
Steven Scoville <sscoville@expr.net>
2020-08-04 08:23:46 -0500

    Loader setup

---
+++ bull-project/webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.(scss|css)$/,
+        loaders: [
+          MiniCssExtractPlugin.loader,
+          // 'style-loader',
+          'css-loader',
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'app.css'
+    }),
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
